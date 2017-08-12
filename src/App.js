c9b8554e5e46ffd9a13d7b9300969a64369fd54d
Chad Lundgren <chad.lundgren@gmail.com>
2017-08-12 12:58:49 -0600

    GraphQL query

--- src/App.js
+++ src/App.js
@@ -1,21 +1,79 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
+import BigCalendar from 'react-big-calendar'
+import moment from 'moment'
+import 'react-big-calendar/lib/css/react-big-calendar.css'
+
+// Setup the localizer by providing the moment (or globalize) Object
+// to the correct localizer.
+BigCalendar.momentLocalizer(moment); // or globalizeLocalizer
 
 class App extends Component {
+
+ static defaultProps = {
+    className: ''
+  }
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      events: []
+    }
+  }
+
   render() {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h1>Calendar</h1>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="big-calendar">
+          <BigCalendar
+            events={this.state.events}
+            startAccessor='startDate'
+            endAccessor='endDate'
+          />
+        </div>
       </div>
-    );
+    )
   }
+
+
+  componentDidMount() {
+    fetch('http://localhost:3000/date_book/api', {
+      method: 'post',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        query: 
+        `{
+          event_occurrences {
+            start,
+            end,
+            url,
+            popover_url,
+            event {
+              id,
+              name,
+              slug,
+              all_day,
+              css_class,
+              text_color,
+              background_color
+              border_color
+            } 
+          }
+        }`,
+      })
+    })
+      .then((res) => res.json())
+      .then((data) => data.data.event_occurrences)
+      .then((events) => this.setState({ events }))
+  }
+
 }
 
-export default App;
+export default App
