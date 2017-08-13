57e5107f8b952b601e460587f0a18aad7f22cfb4
Chad Lundgren <chad.lundgren@gmail.com>
2017-08-13 11:53:37 -0600

    WIP: Mapping data from GraphQL

--- src/calendars.js
+++ src/calendars.js
@@ -26,7 +26,7 @@ class Calendars extends Component {
     return (
       <div className="calendars">
         <BigCalendar
-          events={[]}
+          events={this.state.events}
           startAccessor='startDate'
           endAccessor='endDate'
         />
@@ -34,70 +34,70 @@ class Calendars extends Component {
     )
   }
   
-  // componentDidMount() {
-  //   fetch('http://localhost:3000/date_book/api', {
-  //     method: 'post',
-  //     headers: {
-  //       Accept: 'Calendarlication/json',
-  //       'Content-Type': 'Calendarlication/json',
-  //     },
-  //     body: JSON.stringify({
-  //       query: 
-  //       `{
-  //         event_occurrences {
-  //           start,
-  //           end,
-  //           url,
-  //           popover_url,
-  //           event {
-  //             id,
-  //             name,
-  //             slug,
-  //             all_day,
-  //             css_class,
-  //             text_color,
-  //             background_color
-  //             border_color
-  //           } 
-  //         }
-  //       }`,
-  //     })
-  //   })
-  //     .then((res) => res.json())
-  //     .then((data) => data.data.event_occurrences)
-  //     .then((events) => this.setState({ events }))
-  // }
+  componentDidMount() {
+    fetch('http://localhost:3000/date_book/api', {
+      method: 'post',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        query: 
+        `{
+          event_occurrences {
+            start,
+            end,
+            url,
+            popover_url,
+            event {
+              id,
+              name,
+              slug,
+              all_day,
+              css_class,
+              text_color,
+              background_color
+              border_color
+            } 
+          }
+        }`,
+      })
+    })
+      .then((res) => res.json())
+      .then((data) => { 
+        let event_occurrences = data.data.event_occurrences.map((occurrence) => {
+          let event = occurrence.event
+          let className = ''
+          
+          if(event.css_class !== null) {
+            className = event.css_class;
+          }
 
+          if(event.all_day == true) {
+              className = className + ' all-day';
+          } else {
+              className = className + ' part-day';
+          }
+          return({
+              id: event.id,
+              title: event.name,
+              description: event.description,
+              className: className,
+              textColor: event.text_color,
+              backgroundColor: event.background_color,
+              borderColor: event.border_color,
+              allDay: event.all_day,
+              url: occurrence.url,
+              popover_url: occurrence.popover_url,
+              start: occurrence.start,
+              end: occurrence.end
+          });
+        })
+        console.log(event_occurrences)
+        return event_occurrences
+      } )
+      .then((events) => this.setState({ events }))
+  }
 }
 
 export default Calendars
-
-
-
-
-
-    // event = occurrence.event;
-    // if(event.css_class == null) {
-    //     className = '';
-    // } else {
-    //     className = event.css_class;
-    // }
-    // if(event.all_day == true) {
-    //     className = className + ' all-day';
-    // } else {
-    //     className = className + ' part-day';
-    // }
-    // return({
-    //     id: event.id,
-    //     title: event.name,
-    //     description: event.description,
-    //     className: className,
-    //     textColor: event.text_color,
-    //     backgroundColor: event.background_color,
-    //     borderColor: event.border_color,
-    //     allDay: event.all_day,
-    //     url: occurrence.url,
-    //     popover_url: occurrence.popover_url,
-    //     start: occurrence.start,
-    //     end: occurrence.end
-    // });
