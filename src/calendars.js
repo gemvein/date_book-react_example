99784b2c279f353ad9eb930156fabc35798612b5
Karen Lundgren <karen.e.lundgren@gmail.com>
2017-08-31 08:48:36 -0600

    Fixed colors for calendars

--- src/calendars.js
+++ src/calendars.js
@@ -1,33 +1,25 @@
 import './calendars.css'
-import React, { Component } from 'react'
+import React, {Component} from 'react'
 import BigCalendar from 'react-big-calendar'
 import moment from 'moment'
 import 'react-big-calendar/lib/css/react-big-calendar.css'
-import { QueryRenderer, graphql } from 'react-relay'
+import {QueryRenderer, graphql} from 'react-relay'
 import environment from './environment'
 
 // Setup the localizer by providing the moment (or globalize) Object
 // to the correct localizer.
 BigCalendar.momentLocalizer(moment) // or globalizeLocalizer
 
-function StyledEvent({ event }) {
-  let style = ''
-  if (event.textColor != null) {
-    style = `${style} color: ${event.textColor};`
-  }
-  if (event.backgroundColor != null) {
-    style = `${style} background-color: ${event.backgroundColor};`
-  }
-  if (event.borderColor != null) {
-    style = `${style} border-color: ${event.borderColor};`
-  }
+function StyledEventWrapper({event}) {
+  const dayClassName = (event.all_day === true) ? 'rbc-event-allday' : 'rbc-event-partday'
+
   return (
-      <span className={event.className} style={style}>
+    <div className={`rbc-event ${dayClassName} ${event.className}`}
+         style={event.style}>
       <strong>
-      {event.title}
+        {event.title}
       </strong>
-        { event.desc && (':  ' + event.desc)}
-    </span>
+    </div>
   )
 }
 
@@ -36,11 +28,11 @@ class Calendars extends Component {
 
   render() {
     return (
-        <div className="calendars">
+      <div className="calendars">
 
-          <QueryRenderer
-              environment={environment}
-              query={graphql`
+        <QueryRenderer
+          environment={environment}
+          query={graphql`
                 query calendarsQuery {
                   event_occurrences {
                     start,
@@ -65,77 +57,71 @@ class Calendars extends Component {
               `}
 
 
+          // query={graphql`
+          //     query pageQuery($pageSlug: String!, $userSlug: String!) {
+          //       page(slug: $pageSlug, user_id: $userSlug) {
+          //         name,
+          //         body
+          //       }
+          //     }
+          //   `}
 
-              // query={graphql`
-              //     query pageQuery($pageSlug: String!, $userSlug: String!) {
-              //       page(slug: $pageSlug, user_id: $userSlug) {
-              //         name,
-              //         body
-              //       }
-              //     }
-              //   `}
+          // variables={{
+          //   userSlug: 'john-doe',
+          //   pageSlug: 'index'
+          // }}
+          render={({error, props}) => {
+            if (error) {
+              return <div>{error.message}</div>
+            } else if (props) {
+              const events = props.event_occurrences.map((occurrence) => {
+                let event = occurrence.event
+                let classNames = []
 
-              // variables={{
-              //   userSlug: 'john-doe',
-              //   pageSlug: 'index'
-              // }}
-              render={({error, props}) => {
-                if (error) {
-                  return <div>{error.message}</div>
-                } else if (props) {
-                  const events = props.event_occurrences.map((occurrence) => {
-                    let event = occurrence.event
-                    let className = ''
-
-                    if (event.css_class !== null) {
-                      className = event.css_class
-                    }
-                    if (event.calendar.css_class !== null) {
-                      className = `${className} ${event.css_class}`
-                    }
-
-                    if (event.all_day === true) {
-                      className = `${className} event event-all-day`
-                    } else {
-                      className = `${className} event event-part-day`
-                    }
-                    return ({
-                      id: event.id,
-                      title: event.name,
-                      description: event.description,
-                      className: className,
-                      textColor: event.text_color,
-                      backgroundColor: event.background_color,
-                      borderColor: event.border_color,
-                      allDay: event.all_day,
-                      url: occurrence.url,
-                      popover_url: occurrence.popover_url,
-                      startDate: moment(occurrence.start).toDate(),
-                      endDate: moment(occurrence.end).toDate()
-                    })
-                  })
-
-                  return <BigCalendar
-                      events={events}
-                      startAccessor='startDate'
-                      endAccessor='endDate'
-                      titleAccessor='title'
-                      allDayAccessor='allDay'
-
-                      components={
-                        {
-                          month: {
-                            event: StyledEvent
-                          }
-                        }
-                      }
-                  />
+                if (event.css_class !== null) {
+                  classNames.push(event.css_class)
                 }
-                return <div>Loading</div>
-              }}
-          />
+                if (event.calendar.css_class !== null) {
+                  classNames.push(event.calendar.css_class)
+                }
 
-        </div>
+                return ({
+                  id: event.id,
+                  title: event.name,
+                  description: event.description,
+                  className: classNames.join(' '),
+                  style: {
+                    color: event.textColor,
+                    backgroundColor: event.calendar.background_color,
+                    borderColor: event.calendar.border_color
+                  },
+                  allDay: event.all_day,
+                  url: occurrence.url,
+                  popover_url: occurrence.popover_url,
+                  startDate: moment(occurrence.start).toDate(),
+                  endDate: moment(occurrence.end).subtract(1, 'seconds').toDate()
+                })
+              })
+
+              return <BigCalendar
+                events={events}
+                startAccessor='startDate'
+                endAccessor='endDate'
+                titleAccessor='title'
+                allDayAccessor='allDay'
+
+                components={
+                  {
+                    eventWrapper: StyledEventWrapper
+                  }
+                }
+              />
+            }
+            return <div>Loading</div>
+          }}
+        />
+
+      </div>
     )
   }
 
