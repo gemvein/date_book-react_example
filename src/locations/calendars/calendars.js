2728bf225dcb3e35317b3671343923f161e1f734
Chad Lundgren <chad.lundgren@gmail.com>
2017-12-09 15:13:42 -0700

    Upgrade react to 16, switch to Dayz
    More work needed on colors, description, etc

--- src/locations/calendars/calendars.js
+++ src/locations/calendars/calendars.js
@@ -2,9 +2,10 @@ import React, {Component} from 'react'
 import ReactDOM from 'react-dom'
 import {QueryRenderer, graphql} from 'react-relay'
 import environment from '../../environment'
-import EventCalendar from 'react-event-calendar'
 import moment from 'moment'
 import {Grid, Menu, Button, Popup, Popover, Modal, Dimmer, Loader} from 'semantic-ui-react'
+import Dayz from 'dayz'
+import 'dayz/dist/dayz.css'
 
 const dataFormat = 'YYYY-MM-DD'
 const monthNames = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December',]
@@ -107,8 +108,8 @@ class Calendars extends Component {
         <div className="calendars">
 
           <QueryRenderer
-            environment={environment}
-            query={graphql`
+              environment={environment}
+              query={graphql`
               query calendarsQuery {
                 event_occurrences {
                   start,
@@ -132,92 +133,93 @@ class Calendars extends Component {
               }
             `}
 
-            render={({error, props}) => {
-              if (error) {
-                return <div>{error.message}</div>
-              } else if (props) {
+              render={({error, props}) => {
+                if (error) {
+                  return <div>{error.message}</div>
+                } else if (props) {
 
-                const events = props.event_occurrences.map((occurrence) => {
-                  let event = occurrence.event
-                  let classNames = []
+                  const events = new Dayz.EventsCollection(
+                      props.event_occurrences.map((occurrence) => {
+                        let event = occurrence.event
+                        let classNames = []
 
-                  if (event.css_class !== null) {
-                    classNames.push(event.css_class)
-                  }
-                  if (event.calendar.css_class !== null) {
-                    classNames.push(event.calendar.css_class)
-                  }
+                        if (event.css_class !== null) {
+                          classNames.push(event.css_class)
+                        }
+                        if (event.calendar.css_class !== null) {
+                          classNames.push(event.calendar.css_class)
+                        }
 
-                  return ({
-                    id: event.id,
-                    title: event.name,
-                    description: event.description,
-                    eventClasses: classNames.join(' '),
-                    data: {
-                      allDay: event.all_day,
-                      url: occurrence.url,
-                      popover_url: occurrence.popover_url,
-                      style: {
-                        color: event.textColor,
-                        backgroundColor: event.calendar.background_color,
-                        borderColor: event.calendar.border_color
-                      }
-                    },
-                    start: moment(occurrence.start).format(dataFormat),
-                    end: moment(occurrence.end).subtract(1, 'seconds').format(dataFormat),
+                        return ({
+                          id: event.id,
+                          content: event.name,
+                          // description: event.description,
+                          // eventClasses: classNames.join(' '),
+                          // data: {
+                          //   allDay: event.all_day,
+                          //   url: occurrence.url,
+                          //   popover_url: occurrence.popover_url,
+                          //   style: {
+                          //     color: event.textColor,
+                          //     backgroundColor: event.calendar.background_color,
+                          //     borderColor: event.calendar.border_color
+                          //   }
+                          // },
+                          range: moment.range(
+                              moment(occurrence.start).format(dataFormat),
+                              moment(occurrence.end).subtract(1, 'seconds').format(dataFormat)
+                          ),
+                          // TODO: Map this to API values.
+                          // TODO: Figure out how many colors we want to implement in the stylesheet
+                          colorIndex: "blue"
+                        })
+                      })
+                  )
 
-                  })
-                })
+                  return (
+                      <Grid>
+                        <Grid.Row>
+                          <Grid.Column width={8}>
+                            <Menu>
+                              <Button onClick={this.handlePreviousMonth}>&lt;</Button>
+                              <Button onClick={this.handleNextMonth}>&gt;</Button>
+                              <Button onClick={this.handleToday}>Today</Button>
+                            </Menu>
+                          </Grid.Column>
+                          <Grid.Column width={8}>
+                            <h1>{this.getHumanDate()}</h1>
+                          </Grid.Column>
+                        </Grid.Row>
+                        <Grid.Row>
+                          <Grid.Column width={16}>
+                            <Dayz
+                                display='month'
+                                date={this.state.moment}
+                                events={events}
+                                onDayClick={this.handleDayClick}
+                                onEventClick={this.handleEventClick}
+                            />
+                          </Grid.Column>
+                        </Grid.Row>
+                      </Grid>
+                  )
+                }
 
                 return (
-                  <Grid>
-                    <Grid.Row>
-                      <Grid.Column width={8}>
-                        <Menu>
-                          <Button onClick={this.handlePreviousMonth}>&lt;</Button>
-                          <Button onClick={this.handleNextMonth}>&gt;</Button>
-                          <Button onClick={this.handleToday}>Today</Button>
-                        </Menu>
-                      </Grid.Column>
-                      <Grid.Column width={8}>
-                        <h1>{this.getHumanDate()}</h1>
-                      </Grid.Column>
-                    </Grid.Row>
-                    <Grid.Row>
-                      <Grid.Column width={16}>
-
-                        <EventCalendar
-                            month={this.state.moment.month()}
-                            year={this.state.moment.year()}
-                            events={events}
-                            onEventClick={this.handleEventClick}
-                            onEventMouseOver={this.handleEventMouseOver}
-                            onEventMouseOut={this.handleEventMouseOut}
-                            onDayClick={this.handleDayClick}
-                            maxEventSlots={10}
-                        />
-
-                      </Grid.Column>
-                    </Grid.Row>
-                  </Grid>
+                    <Dimmer inverted active>
+                      <Loader active size='massive' inverted>Loading</Loader>
+                    </Dimmer>
                 )
-              }
-
-              return (
-                <Dimmer inverted active>
-                  <Loader active size='massive' inverted>Loading</Loader>
-                </Dimmer>
-              )
-            }}
+              }}
           /> {/* End QueryRenderer */}
 
           <Popup
-            show={this.state.showPopover}
-            rootClose
-            onHide={() => this.setState({showPopover: false,})}
-            placement="top"
-            container={this}
-            target={this.state.popoverTarget}>
+              show={this.state.showPopover}
+              rootClose
+              onHide={() => this.setState({showPopover: false,})}
+              placement="top"
+              container={this}
+              target={this.state.popoverTarget}>
             {/*<Popover id="event">{this.state.popupTitle}</Popover>*/}
           </Popup>
 
